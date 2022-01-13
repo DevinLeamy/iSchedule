13469bb401e32781ff9991a8a400987a542b17dc
Devin Leamy <devinleamy@gmail.com>
2022-01-12 23:14:52 -0700

    made it more pretty - still bad code - need to fix that

--- src/components/PageOne/PageOne.tsx
+++ src/components/PageOne/PageOne.tsx
@@ -7,46 +7,80 @@ import Calendar from "../../components/Calendar/Calendar";
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Button from "@mui/material/Button";
-import TimezoneSelect, { ITimezone } from "react-timezone-select";
-// import Intl from "intl";
-import Kalend, { CalendarView } from 'kalend';
-import 'kalend/dist/styles/index.css'; 
+import Radio from '@mui/material/Radio';
 
+import TimezoneSelect, { ITimezone, allTimezones } from "react-timezone-select";
+
+
+let MONTH_C: string = "MONTH_C";
+let WEEK_C: string = "WEEK_C";
 
 const PageOne: React.FC = () => {
-  // const [selectedTimezone, setSelectedTimezone] = useState<ITimezone>(
-  //   Intl.DateTimeFormat().resolvedOptions().timeZone
-  // );
+  const [selectedTimezone, setSelectedTimezone] = useState<ITimezone>(
+    Intl.DateTimeFormat().resolvedOptions().timeZone
+  );
 
-  
+  const [calendarType, setCalendarType] = React.useState<string>(WEEK_C);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCalendarType(event.target.value);
+  };
+
 
   return (
     <Page>
       <Header content="Instantly schedule an event"/>
       <div className='spacer'/>
       <ContentBox>
-        <Box>
-          <TextField id="standard-basic" label="Event name" variant="outlined" style={{minWidth: 400}}/>
-        </Box>
+        <div className='h-center-contents'>
+          <TextField 
+            // id="standard-basic" 
+            label="" 
+            variant="standard" 
+            placeholder="Your event name"
+            style={{minWidth: "60%"}}
+            inputProps={{style: {
+              textAlign: "center", 
+              fontSize: 30
+            }}}
+            InputLabelProps={{style: {
+              visibility: "hidden"
+            }}}
+          />
+        </div>
         <div className="spacer"/>
-        <h4>Week - Month</h4>
+        <div className="c-type-toggle-container">
+          <span className="c-type-label">Week</span>
+          <Radio
+            checked={calendarType === WEEK_C}
+            onChange={handleChange}
+            value={WEEK_C}
+          />
+          <span className="c-type-label">Month</span>
+          <Radio
+            checked={calendarType === MONTH_C}
+            onChange={handleChange}
+            value={MONTH_C}
+          />
+        </div>
+        <div className="spacer"/>
         <Box>
-          Timezone select
-          {/* <TimezoneSelect 
+          Select your time zone 
+          {/* npm run build && serve -s build */}
+          <TimezoneSelect 
             value={selectedTimezone}
             onChange={setSelectedTimezone}
             timezones={{...allTimezones}}
-          />  */}
+          /> 
         </Box>
+        <div className="spacer"/>
         <div className='calendar-container'>
           <Calendar 
             days={7}
           />
         </div>
-        <div>
-          Selected date
-        </div>
-        <div className="next-page-btn-container">
+        {/* <div className="next-page-btn-container"> */}
+        <div className="next-page-btn-container h-center-contents">
           <Button variant="contained">
             + Create the event
           </Button>
